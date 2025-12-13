9e8e998bfd2322ca5c10d634c22cea616fddb277
l-dhias <rpleoddiass@gmail.com>
2025-12-12 21:11:22 -0300

    Criando Tag e o button

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,15 +1,27 @@
 import { Imagem, Titulo, Precos } from './styles'
 
 import bannerImg from '../../assets/images/banner-homem-aranha.png'
+import Tag from '../Tag'
+import Button from '../Button'
 
 const Banner = () => (
   <Imagem style={{ backgroundImage: `url(${bannerImg})` }}>
     <div className="container">
-      <Titulo>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Titulo>
-      <Precos>
-        De <span>R$ 250,00</span> <br />
-        Por apenas R$ 99,00
-      </Precos>
+      <Tag size="big">Destaque do dia</Tag>
+      <div>
+        <Titulo>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Titulo>
+        <Precos>
+          De <span>R$ 250,00</span> <br />
+          Por apenas R$ 99,00
+        </Precos>
+      </div>
+      <Button
+        type="link"
+        to="/produtos"
+        title="Clique aqui para aproveitar está oferta"
+      >
+        Aproveitar
+      </Button>
     </div>
   </Imagem>
 )
